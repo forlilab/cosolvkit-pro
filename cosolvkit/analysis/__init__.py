8870a63b7ba9adf94bdd71ecd305b7ed4081bf11
manu <manu.llanos@gmail.com>
2026-06-29 22:48:20 -0700

    refactor: re-export public analysis API from canonical new module paths

--- cosolvkit/analysis/__init__.py
+++ cosolvkit/analysis/__init__.py
@@ -1,13 +1,12 @@
-from .analysis import Report
-from .analysis_config import AnalysisConfig
+from .report import Report
+from .config import AnalysisConfig
 from .multi_report import MultiReport
-from .density_analysis import GridAnalysis
-from .hotspots_detection import HotspotDetector, BindingSite
-from .pocket_properties import (
-    PocketPropertyCalculator, PocketResidue,
-    compute_composite_score, set_residue_embeddings,
-)
-from .consensus_detection import CrossProbeConsensusDetector, ConsensusSite
+from .core.grid import GridAnalysis
+from .sites.detect import HotspotDetector
+from .core.models import BindingSite, ConsensusSite, PocketResidue
+from .sites.properties import PocketPropertyCalculator, set_residue_embeddings
+from .sites.consensus import CrossProbeConsensusDetector
+from .core.scoring import compute_composite_score
 
 __all__ = [
     "Report", "AnalysisConfig", "MultiReport",
